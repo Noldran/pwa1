2d38822ec10e33ba7b61fcc1cf00ed7a44ff500e
Noldran <sclaud81@gmail.com>
2023-01-12 14:57:28 +0100

    Updates

---
+++ precache-manifest.ba6b7b2a5e7917ed2b439f551a30ce69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8f3b2d1b7965694ef1c",
+    "url": "/pwa1/static/css/main.51e0108e.chunk.css"
+  },
+  {
+    "revision": "b8f3b2d1b7965694ef1c",
+    "url": "/pwa1/static/js/main.9d002d8d.chunk.js"
+  },
+  {
+    "revision": "a267895e160248f63549",
+    "url": "/pwa1/static/js/runtime~main.99d932d0.js"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/pwa1/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/pwa1/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pwa1/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a68ceb977b9754aa13666ab631e80448",
+    "url": "/pwa1/index.html"
+  }
+];
